842d42a258dc9bfb765a33cd796f57842df1f723
Bruno Croci <crocidb@gmail.com>
2016-09-10 22:34:33 -0400

    added the concept of Hearts

--- game.js
+++ game.js
@@ -68,7 +68,9 @@ var sex_orient = [
     { w: 0.15, t: "Bisexual" },       // 2
 ];
 
-var total_weeks = 1;
+var week_total = 5;
+var week_price = 10;
+var week_price_increase = 5;
 
 var texts = {
     date_conf: "<p>Do you want to match <b>$1</b> and <b>$2</b>?</p><p>After that they will go to a date and then you will get a date report.</p>",
@@ -76,12 +78,14 @@ var texts = {
     bonus_conf: "<p>Are you sure you want to swap the available people? You have <b>$1</b> bonus left.</p>",
     bonus_not: "<p>You can't do that because you don't have any bonus left.</p>",
     week_started: "<p>Week <b>$1</b> just started. You need to match <b>$2</b> people in the left list to go to the next one.</p>",
-    welcome_help: "<div class=\"inner-object results-box\"><p>Phosphorus Dating is a very modern dating website, our matching algorithm was considered one of the most advanced ever created. We achieved more than <b>95%</b> of successful couples. But recently something happened and for some reason the accuracy is below <b>25%</b>, this is why we activated the manual mode and you're here.</p><p>You have <b>" + total_weeks + "</b> weeks to match couples based on their <b>gender</b>, <b>sexual orientation</b>, <b>age</b> and <b>interests</b>. Every week you will have to find a good couple for the people in the left list of the Phosphorus Dating program. Pick the right person on the right and match the couple. If you can't find a good match, you can try to ask more people, but be careful, it's limited.</p><p>When you match a couple, they will go out on a date and you can keep up with the progress on the lower left date list on the program. After that finishes you'll get a report.</p></div>",
+    welcome_help: "<div class=\"inner-object results-box\"><p>Phosphorus Dating is a very modern dating website, our matching algorithm was considered one of the most advanced ever created. We achieved more than <b>95%</b> of successful couples. But recently something happened and for some reason the accuracy is below <b>25%</b>, this is why we activated the manual mode and you're here.</p><p>You have <b>" + week_total + "</b> weeks to match couples based on their <b>gender</b>, <b>sexual orientation</b>, <b>age</b> and <b>interests</b>. Every week you will have to find a good couple for the people in the left list of the Phosphorus Dating program. Pick the right person on the right and match the couple. If you can't find a good match, you can try to ask more people, but be careful, it's limited.</p><p>When you match a couple, they will go out on a date and you can keep up with the progress on the lower left date list on the program. After that finishes you'll get a report.</p></div>",
     finish: "You finished the period with <b>$1%</b>.",
     finish_good: "That's remarkable! Congratulations. We will have to boost our algorithm to keep up with that mark.",
     finish_ok: "Good job. We hope our algorithm is finally fixed.",
     finish_bad: "That wasn't as good as we initially thought, but better than using a broken algorithm. Thanks for the effort.",
-    finish_glitch: "That's A̶̦̒A̴a̽a̳̾A̶͖A̎͊A̿a͚͉͠A͠A̋a̦̩ã̷̸AͣA̳ͤ́A̩̥Ä̵́ͅ0͆0̋0̷̸̋f̰a̷͕͒ c͙͗f͑ b̡̕c͓̟̓ 0̵̇͡3͎̞͘ 0̞͑1̯̗͜ 2̲3ͧc͔5̴̞̥ AD"
+    finish_glitch: "That's A̶̦̒A̴a̽a̳̾A̶͖A̎͊A̿a͚͉͠A͠A̋a̦̩ã̷̸AͣA̳ͤ́A̩̥Ä̵́ͅ0͆0̋0̷̸̋f̰a̷͕͒ c͙͗f͑ b̡̕c͓̟̓ 0̵̇͡3͎̞͘ 0̞͑1̯̗͜ 2̲3ͧc͔5̴̞̥ AD",
+    week_next: "Next week ($1&#10084;)",
+    not_enough_hearts: "<p>You finished the week with <b>$1 &#10084</b>; and needed <b>$2 #10084;</b> to go to the next one.<p>"
 };
 
 var match_category = [
@@ -387,7 +391,7 @@ var Match = function(p1, p2) {
         }
 
         mtext += '</ul>';
-        mtext += '<p>You scored <b>' + this.score + '</b>.</p>';
+        mtext += '<p>Hearts &#10084; earned <b>' + this.score + '</b>.</p>';
         mtext += '</div>';
         showDialogOk("Date finished: " + cat.title, mtext, this.killCurrentMatch.bind(this));
     };
@@ -591,7 +595,7 @@ var Game = function() {
 
     this.currentPeople = [0, 0];
 
-    this.bonus = 3;
+    this.weekPrice = week_price;
 
     // DOM elements
     this.dateWeek = gId("dateWeek");
@@ -600,6 +604,8 @@ var Game = function() {
     this.dateRating = gId("dateRating");
     this.dateScore = gId("dateScore");
     this.dateBonus = gId("dateBonus");
+    
+    this.nextWeekButton = gId("nextWeek");
 
     this.start = function () {
         var txt = "<p>Welcome to <b>Phosphorus Dating</b>. You are logged in as <span style=\"color: green\">cecilia.dent</span>.</p>";
@@ -615,7 +621,7 @@ var Game = function() {
     this.startWeek = function() {
         this.week++;
 
-        if (this.week > total_weeks) {
+        if (this.week > week_total) {
             this.endOfGame();
             return;
         }
@@ -630,19 +636,22 @@ var Game = function() {
         this.renderDetails();
         this.updateDateData();
 
+        this.nextWeekButton.innerHTML = "<span>" + texts.week_next.replace("$1", this.weekPrice) + "</span>";
+        this.nextWeekButton.className += " disabled";
+
         var txt = texts.week_started.replace("$1", this.week).replace("$2", this.opeople.length);
         showDialogOk("Week", txt);
     };
 
     this.startWeekPeople = function() {
         this.opeople = [];
-        var a = 3 + Math.floor((this.week) / 2);
+        var a = 3 + Math.floor((this.week) / 3);
         for (var i = 0; i < a; i++) {
             var p = new Person();
             this.opeople.push(p);
         }
 
-        if (this.week == total_weeks) {
+        if (this.week == week_total) {
             this.opeople.push(cecilia_dent);
         }
 
@@ -651,7 +660,7 @@ var Game = function() {
 
     this.randomPeople = function() {
         this.people = [];
-        for (var i = 0; i < 20; i++)
+        for (var i = 0; i < 7; i++)
         {
             var p = new Person();
             this.people.push(p);
@@ -672,7 +681,6 @@ var Game = function() {
         this.dateCouples.innerHTML = this.goodWeekMatches + '/' + this.finishedMatches.length;
         this.dateRating.innerHTML = rating;
         this.dateScore.innerHTML = this.totalScore < 0 ? "Too low" : this.totalScore;
-        this.dateBonus.innerHTML = this.bonus;
     };
 
     this.drawPeople = function() {
@@ -706,20 +714,11 @@ var Game = function() {
         p2.innerHTML = peopleDetail(this.currentPeople[1]);
     };
 
-    this.useBonus = function() {
-        this.bonus--;
-        this.randomPeople();
-
-        this.updateDateData();
-        this.drawPeople();
-        this.renderDetails();
-    };
-
-    this.more = function() {
-        if (this.bonus > 0) {
-            showDialogOk("Bonus", texts.bonus_conf.replace("$1", this.bonus), this.useBonus.bind(this), true);
-        } else {
-            showDialogOk("Bonus", texts.bonus_not);
+    this.nextWeek = function() {
+        if (this.opeople.length == 0 && this.matches == 0) {
+            this.totalScore -= this.weekPrice;
+            this.weekPrice += week_price_increase;
+            this.finishWeek();
         }
     };
 
@@ -779,7 +778,16 @@ var Game = function() {
         console.log(this);
 
         if (this.opeople.length == 0 && this.matches == 0) {
-            setTimeout(this.finishWeek.bind(this), 400);
+            if (this.totalScore >= week_price) {
+                this.nextWeekButton.className = this.nextWeekButton.className.replace(/ disabled/g, '');
+            } else {
+                var msg = texts.not_enough_hearts.replace("$1", this.totalScore).replace("$2", this.weekPrice);
+                showDialogOk("Not enough hearts", msg, function() {
+                    gameConsole.init(gId("consoleEndOfGameNotEnoughHearts").innerHTML, function() {
+                        location.reload();
+                    });
+                });
+            }
         }
     };
 
